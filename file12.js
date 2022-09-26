df195b58c122236fc7a5f6ead81166b2feec0021
Suhani Upadhyay <113186758+Suhani-Upadhyay@users.noreply.github.com>
2022-09-26 22:48:54 +0530

    Add files via upload

---
+++ file12.js
@@ -0,0 +1,36 @@
+// . boolean & comparsion opertor
+
+ let num1 = 5;
+ let num2= 7;
+
+ console.log(num1>num2);
+ 
+ console.log(num1<num2)
+ console.log(num1<=num2)
+
+
+//  == & ===
+// ==---- ye sirf value ka dhyan rhega datatypes ka dhyan nhi rhega(only for JS)
+ let num3="6";
+ let num4= 6;
+ console.log(num3==num4);
+
+//  ===  ----- ye dhyan rhega datatypes ka b
+
+console.log(num3===num4);
+
+
+// !=  vs 1==
+
+let num5="6";
+ let num6= 6;
+ console.log(num5 !=num6);
+ console.log(num5 !== num6);
+
+
+
+
+
+
+
+
