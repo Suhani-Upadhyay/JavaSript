81f2ba4752a6b1aab25254e82822e89d4c0789ae
Suhani Upadhyay <113186758+Suhani-Upadhyay@users.noreply.github.com>
2022-10-02 09:51:05 +0530

    Add files via upload

---
+++ file19.js
@@ -0,0 +1,23 @@
+// loop statment
+
+// 0 to 9 print
+// while loop
+
+let i=0;
+while(i<=10){
+    console.log(i)
+    i++
+}
+
+// example
+
+let  i1=0;
+let sum=0;
+
+while(i1<=10){
+    sum=sum+i1;
+    
+    i1++;
+}
+
+console.log(sum);
