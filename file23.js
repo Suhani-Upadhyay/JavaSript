4cc5b628029b5dd8c27eabeb66ba95c7bcfc5ba9
Suhani Upadhyay <113186758+Suhani-Upadhyay@users.noreply.github.com>
2022-10-06 22:53:02 +0530

    Add files via upload

---
+++ file23.js
@@ -0,0 +1,27 @@
+//  intro to aarays---collection of elements (reference type --- object) or
+//  ordered collection of items
+
+let fruits = ["apple", "mango", "orange" ];
+let number = [1,2,3,4];
+let mixed = [1,2, "string", null, undefined]
+
+console.log(fruits);
+
+console.log(fruits[1]);
+
+console.log(number);
+
+console.log(mixed);
+
+let fruits1 = ["apple", "mango", "orange" ];
+ console.log(fruits1);
+
+ fruits1[1]= "banana"
+ console.log(fruits1);
+
+ let fruits2 = ["apple", "mango", "orange" ];
+ console.log(typeof fruits2); 
+// object 
+
+ console.log(Array .isArray(fruits2));
+//  true
